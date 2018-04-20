1ee1a95b83b00bbb647faeddf1880d6ac5d911a1
Peggy Rayzis <peggyrayzis@gmail.com>
2018-04-20 03:17:37 -0600

    Boost setup complete

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -16,46 +16,11 @@ PART 2:
 */
 
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 
 import MovieTile from './MovieTile';
 import Filter from './Filter';
 
-const MovieInfoFragment = gql`
-  fragment MovieInfo on Movie {
-    id
-    title
-    isLiked
-    score
-    overview
-    popularity
-    poster
-    cast {
-      name
-      id
-      photo
-    }
-  }
-`;
-
-const GET_MOVIES = gql`
-  query movieList($sort: SORT_TYPE, $page: Int!) {
-    movies(sort: $sort, page: $page) {
-      ...MovieInfo
-    }
-  }
-  ${MovieInfoFragment}
-`;
-
-export const GET_LIKED_MOVIES = gql`
-  {
-    likes {
-      ...MovieInfo
-    }
-  }
-  ${MovieInfoFragment}
-`;
+import data from '../data';
 
 export default class MovieList extends Component {
   state = { sort: 'POPULARITY' };
@@ -64,61 +29,16 @@ export default class MovieList extends Component {
 
   render = () => {
     return (
-      <Query
-        query={this.state.sort !== 'LIKES' ? GET_MOVIES : GET_LIKED_MOVIES}
-        fetchPolicy={
-          this.state.sort !== 'LIKES' ? 'cache-first' : 'cache-and-network'
-        }
-        variables={
-          this.state.sort !== 'LIKES'
-            ? {
-                showLikes: false,
-                page: 1,
-                sort: this.state.sort,
-              }
-            : {}
-        }
-      >
-        {({ loading, error, data, fetchMore }) => {
-          if (loading) return 'Loading...';
-          if (error) return `${error}`;
-
-          return (
-            <div>
-              <Filter
-                onFilterChange={this.onFilterChange}
-                selected={this.state.sort}
-              />
-              {(data.movies || data.likes).map(movie => (
-                <MovieTile key={movie.id} movie={movie} />
-              ))}
-              {data.movies && (
-                <button
-                  onClick={() => {
-                    const newPage = Math.floor(data.movies.length / 20) + 1;
-                    return fetchMore({
-                      variables: {
-                        page: newPage,
-                      },
-                      updateQuery: (previous, { fetchMoreResult }) => {
-                        if (!fetchMoreResult) return previous;
-                        return {
-                          movies: [
-                            ...previous.movies,
-                            ...fetchMoreResult.movies,
-                          ],
-                        };
-                      },
-                    });
-                  }}
-                >
-                  Load More
-                </button>
-              )}
-            </div>
-          );
-        }}
-      </Query>
+      <div>
+        <Filter
+          onFilterChange={this.onFilterChange}
+          selected={this.state.sort}
+        />
+        {(data.movies || data.likes).map(movie => (
+          <MovieTile key={movie.id} movie={movie} />
+        ))}
+        {data.movies && <button onClick={() => {}}>Load More</button>}
+      </div>
     );
   };
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+  movies: [
+    {
+      id: '337167',
+      isLiked: false,
+      overview:
+        'Believing they have left behind shadowy figures from their past, newlyweds Christian and Ana fully embrace an inextricable connection and shared life of luxury. But just as she steps into her role as Mrs. Grey and he relaxes into an unfamiliar stability, new threats could jeopardize their happy ending before it even begins.',
+      popularity: 551.431531,
+      poster: 'https://image.tmdb.org/t/p/w500/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg',
+      score: 6.1,
+      title: 'Fifty Shades Freed',
+    },
+    {
+      id: '269149',
+      isLiked: false,
+      overview:
+        "Determined to prove herself, Officer Judy Hopps, the first bunny on Zootopia's police force, jumps at the chance to crack her first case - even if it means partnering with scam-artist fox Nick Wilde to solve the mystery.",
+      popularity: 314.911786,
+      poster: 'https://image.tmdb.org/t/p/w500/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg',
+      score: 7.7,
+      title: 'Zootopia',
+    },
+    {
+      id: '354912',
+      isLiked: false,
+      overview:
+        "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+      popularity: 245.339298,
+      poster: 'https://image.tmdb.org/t/p/w500/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+      score: 7.8,
+      title: 'Coco',
+    },
+    {
+      id: '284054',
+      isLiked: false,
+      overview:
+        "King T'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne by factions within his own country as well as without.  Using powers reserved to Wakandan kings, T'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister,  members of the Dora Milaje (the Wakandan \"special forces\"), and an American secret agent, to prevent Wakanda from being dragged into a world war.",
+      popularity: 161.978567,
+      poster: 'https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg',
+      score: 7.3,
+      title: 'Black Panther',
+    },
+    {
+      id: '181808',
+      isLiked: false,
+      overview:
+        'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+      popularity: 148.432949,
+      poster: 'https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg',
+      score: 7.1,
+      title: 'Star Wars: The Last Jedi',
+    },
+    {
+      id: '447332',
+      isLiked: false,
+      overview:
+        'A family is forced to live in silence while hiding from creatures that hunt by sound.',
+      popularity: 147.916054,
+      poster: 'https://image.tmdb.org/t/p/w500/nAU74GmpUk7t5iklEp3bufwDq4n.jpg',
+      score: 7.5,
+      title: 'A Quiet Place',
+    },
+  ],
+};
+
+export default data;
