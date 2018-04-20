ea02f1926931b69db1a46a939572346e1f06beb0
Peggy Rayzis <peggyrayzis@gmail.com>
2018-04-20 03:21:12 -0600

    Blank slate

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
 import App from './App';
 
-const client = new ApolloClient({
-  uri: 'https://fullstack-workshop-server.glitch.me/graphql',
-  request: operation => {
-    operation.setContext(context => ({
-      headers: {
-        ...context.headers,
-        authorization: localStorage.getItem('token'),
-      },
-    }));
-  },
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
